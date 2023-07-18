d0a32ca153c2b32b1edaf416f9b9a702b3fe54fc
razakhan3 <raza@pensil.in>
2023-07-18 21:45:49 +0530

    Added banner section on homepage Added video ad section

---
+++ src/component/Home.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Button, Heading, Image, Stack, Text, VStack } from '@chakra-ui/react';
+import './home.css';
+import { Link } from 'react-router-dom';
+import logo from '../assets/images/bg.png';
+import video from '../assets/videos/homepage-add.mp4';
+const Home = () => {
+  return (
+    <section className="home">
+      <div className="container">
+        <Stack
+          direction={['column', 'row']}
+          height="100%"
+          justifyContent={['center', 'space-between']}
+          alignItems="center"
+        >
+          <VStack
+            width="full"
+            alignItems={['center', 'flex-end']}
+            textAlign={['center', 'right']}
+          >
+            <Heading children="UNLEASH YOUR INNER WARRIOR!" size="2xl" />
+            <Text children="Step into the World of MMA, Boxing, Karate, Taekwondo, and More!" />
+            <Link to="/courses">
+              <Button size="lg" colorScheme="yellow">
+                Explore Now
+              </Button>
+            </Link>
+          </VStack>
+          <Image
+            className="logo-animation"
+            boxSize="md"
+            src={logo}
+            objectFit={'contain'}
+          />
+        </Stack>
+      </div>
+      <div className="container2">
+        <video autoPlay controls src={video}></video>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
