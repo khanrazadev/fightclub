bfc7005e57fb9e5b4cb6c0fb5091974533e0dbc5
razakhan3 <raza@pensil.in>
2023-07-20 15:41:58 +0530

    Completed almost all the frontend UI part Added below component -CourseModal -Users

--- src/component/Admin/AdminCourses/AdminCourses.jsx
+++ src/component/Admin/AdminCourses/AdminCourses.jsx
@@ -1,18 +1,180 @@
 import React from 'react';
-import { Box, Grid } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Grid,
+  HStack,
+  Heading,
+  Image,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  useDisclosure,
+} from '@chakra-ui/react';
 import Sidebar from '../Sidebar';
 import cursor from '../../../assets/images/cursor.png';
+import { RiDeleteBin7Fill } from 'react-icons/ri';
+import CourseModal from './CourseModal';
 const AdminCourses = () => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const deleteButtonHandler = () => {};
+  const deleteLectureButtonHandler = () => {};
+  const addLectureHandler = () => {};
+  const coureDetailsHandler = () => {
+    onOpen();
+  };
+  const courses = [
+    {
+      _id: '12',
+      title: 'Basic of mma',
+      description: 'this is the intro my boi',
+      category: 'MMA',
+      poster: {
+        url: 'https://w0.peakpx.com/wallpaper/821/809/HD-wallpaper-notorious-mac-mma-ufc-conor-mcgregor.jpg',
+      },
+      createdBy: 'Raza Khan',
+      views: 123,
+      numOfVideos: 13,
+    },
+    {
+      _id: '12',
+      title: 'Basic of mma',
+      description: 'this is the intro my boi',
+      category: 'MMA',
+      poster: {
+        url: 'https://w0.peakpx.com/wallpaper/821/809/HD-wallpaper-notorious-mac-mma-ufc-conor-mcgregor.jpg',
+      },
+      createdBy: 'Raza Khan',
+      views: 123,
+      numOfVideos: 13,
+    },
+    {
+      _id: '12',
+      title: 'Basic of mma',
+      description: 'this is the intro my boi',
+      category: 'MMA',
+      poster: {
+        url: 'https://w0.peakpx.com/wallpaper/821/809/HD-wallpaper-notorious-mac-mma-ufc-conor-mcgregor.jpg',
+      },
+      createdBy: 'Raza Khan',
+      views: 123,
+      numOfVideos: 13,
+    },
+    {
+      _id: '12',
+      title: 'Basic of mma',
+      description: 'this is the intro my boi',
+      category: 'MMA',
+      poster: {
+        url: 'https://w0.peakpx.com/wallpaper/821/809/HD-wallpaper-notorious-mac-mma-ufc-conor-mcgregor.jpg',
+      },
+      createdBy: 'Raza Khan',
+      views: 123,
+      numOfVideos: 13,
+    },
+  ];
   return (
     <Grid
+      css={{
+        cursor: `url(${cursor}), default`,
+      }}
       minH={'100vh'}
       templateColumns={['1fr', '5fr 1fr']}
-      css={{ cursor: `url(${cursor}), default` }}
     >
-      <Box></Box>
+      <Box p={['0', '8']} overflowX="auto">
+        <Heading
+          textTransform={'uppercase'}
+          children="All Courses"
+          my="16"
+          textAlign={['center', 'left']}
+        />
+
+        <TableContainer w={['100vw', 'full']}>
+          <Table variant={'simple'} size="lg">
+            <TableCaption>All available courses in the database</TableCaption>
+
+            <Thead>
+              <Tr>
+                <Th>Id</Th>
+                <Th>Poster</Th>
+                <Th>Title</Th>
+                <Th>Category</Th>
+                <Th>Creator</Th>
+                <Th isNumeric>Views</Th>
+                <Th isNumeric>Lectures</Th>
+                <Th isNumeric>Action</Th>
+              </Tr>
+            </Thead>
+
+            <Tbody>
+              {courses.map(item => (
+                <Row
+                  coureDetailsHandler={coureDetailsHandler}
+                  deleteButtonHandler={deleteButtonHandler}
+                  key={item._id}
+                  item={item}
+                />
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+
+        <CourseModal
+          isOpen={isOpen}
+          onClose={onClose}
+          id={12}
+          courseTitle={'BAsic of juitusu'}
+          deleteButtonHandler={deleteLectureButtonHandler}
+          addLectureHandler={addLectureHandler}
+          lectures={courses}
+        />
+      </Box>
+
       <Sidebar />
     </Grid>
   );
 };
+
+function Row({ item, coureDetailsHandler, deleteButtonHandler }) {
+  return (
+    <Tr>
+      <Td>#{item._id}</Td>
+
+      <Td>
+        <Image src={item.poster.url} />
+      </Td>
+
+      <Td>{item.title}</Td>
+      <Td textTransform={'uppercase'}>{item.category}</Td>
+      <Td>{item.createdBy}</Td>
+      <Td isNumeric>{item.views}</Td>
+      <Td isNumeric>{item.numOfVideos}</Td>
+
+      <Td isNumeric>
+        <HStack justifyContent={'flex-end'}>
+          <Button
+            onClick={() => coureDetailsHandler(item._id, item.title)}
+            variant={'outline'}
+            color="purple.500"
+          >
+            View Lectures
+          </Button>
+
+          <Button
+            onClick={() => deleteButtonHandler(item._id)}
+            color={'purple.600'}
+          >
+            <RiDeleteBin7Fill />
+          </Button>
+        </HStack>
+      </Td>
+    </Tr>
+  );
+}
 
 export default AdminCourses;
--- src/component/Admin/Users/Users.jsx
+++ src/component/Admin/Users/Users.jsx
@@ -1,18 +1,119 @@
 import React from 'react';
-import { Box, Grid } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Grid,
+  HStack,
+  Heading,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
 import Sidebar from '../Sidebar';
 import cursor from '../../../assets/images/cursor.png';
+import { RiDeleteBin7Fill } from 'react-icons/ri';
 const Users = () => {
+  const deleteButtonHandler = () => {};
+  const updateHandler = () => {};
+  const users = [
+    {
+      _id: '12',
+      name: 'Raza',
+      email: 'abc@gmail.com',
+      role: 'admin',
+      subscription: {
+        status: 'active',
+      },
+    },
+  ];
   return (
     <Grid
+      css={{
+        cursor: `url(${cursor}), default`,
+      }}
       minH={'100vh'}
       templateColumns={['1fr', '5fr 1fr']}
-      css={{ cursor: `url(${cursor}), default` }}
     >
-      <Box></Box>
+      <Box p={['0', '16']} overflowX="auto">
+        <Heading
+          textTransform={'uppercase'}
+          children="All Users"
+          my="16"
+          textAlign={['center', 'left']}
+        />
+
+        <TableContainer w={['100vw', 'full']}>
+          <Table variant={'simple'} size="lg">
+            <TableCaption>All available users in the database</TableCaption>
+
+            <Thead>
+              <Tr>
+                <Th>Id</Th>
+                <Th>Name</Th>
+                <Th>Email</Th>
+                <Th>Role</Th>
+                <Th>Subscription</Th>
+                <Th isNumeric>Action</Th>
+              </Tr>
+            </Thead>
+
+            <Tbody>
+              {users &&
+                users.map(item => (
+                  <Row
+                    deleteButtonHandler={deleteButtonHandler}
+                    updateHandler={updateHandler}
+                    key={item._id}
+                    item={item}
+                  />
+                ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
+
       <Sidebar />
     </Grid>
   );
 };
 
+function Row({ item, updateHandler, deleteButtonHandler }) {
+  return (
+    <Tr>
+      <Td>#{item._id}</Td>
+      <Td>{item.name}</Td>
+      <Td>{item.email}</Td>
+      <Td>{item.role}</Td>
+      <Td>
+        {item.subscription && item.subscription.status === 'active'
+          ? 'Active'
+          : 'Not Active'}
+      </Td>
+
+      <Td isNumeric>
+        <HStack justifyContent={'flex-end'}>
+          <Button
+            onClick={() => updateHandler(item._id)}
+            variant={'outline'}
+            color="purple.500"
+          >
+            Change Role
+          </Button>
+
+          <Button
+            onClick={() => deleteButtonHandler(item._id)}
+            color={'purple.600'}
+          >
+            <RiDeleteBin7Fill />
+          </Button>
+        </HStack>
+      </Td>
+    </Tr>
+  );
+}
 export default Users;
